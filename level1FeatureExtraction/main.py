fbae5ab667c4ab809a131c6cd988513ca7a3277b
the real gabe guo <gzg2104@columbia.edu>
2022-12-11 03:30:04 +0000

    level 1 feature extraction works

--- level1FeatureExtraction/main.py
+++ level1FeatureExtraction/main.py
@@ -10,28 +10,13 @@ import csv
 
 def editImage(inputImg):
     try:
+        imgName = inputImg.split('/')[-1]
         print('processing {}'.format(inputImg))
-        imgBase = inputImg[inputImg.rfind("/"):inputImg.rfind(".")]
-       
-        maskPath = outputPath + imgBase + "_mask.png"
-        enhancedPath = outputPath + imgBase + "_enhanced.png"
-        orientPath = outputPath + imgBase + "_orient.png"
-        ridgePath = outputPath + imgBase + "_ridge.png"
-        #print('generating enhanced img')
-        enhancedImg = levelOneExtraction.imageFilter1(inputImg, outputPath)
-        #print('generating orientation')
+        enhancedPath = inputImg # NOTE: Should have passed enhanced images
+        orientPath = os.path.join(orientDir, imgName)
+        ridgePath = os.path.join(freqDir, imgName)
         orient, orientList = levelOneExtraction.findOrientationPhase(enhancedPath)
-        mask = cv2.imread(maskPath, 0)
-        #print('generating ridge count')
         freq, ridgeCount = levelOneExtraction.findRidgeFlowCount(enhancedPath, orientList)
-        # print(ridgeCount)
-        #print(enhancedImg.shape, 'enhacned img shape')
-        #print(mask.shape, 'mask shape')
-        for y in range(enhancedImg.shape[0]):
-            for x in range(enhancedImg.shape[1]):
-                if mask[y, x] != 255:
-                    orient[y, x] = 255
-                    freq[y, x] = 0
         cv2.imwrite(ridgePath, freq)
         cv2.imwrite(orientPath, orient)
         print("{} Finished Level One Extraction".format(enhancedPath))
@@ -41,11 +26,15 @@ def editImage(inputImg):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Master Run File')
-    parser.add_argument('--src', dest='input', help='Path to folder containing images', type=str)
+    parser.add_argument('--src', dest='input', help='Path to folder containing images (should have been enhanced beforehand)', type=str)
     args = parser.parse_args()
     inputPath = args.input
     outputPath = os.path.join(inputPath, '../l1_feature_extractions')
     os.makedirs(outputPath, exist_ok=True)
+    orientDir = os.path.join(outputPath, 'orient')
+    os.makedirs(orientDir, exist_ok=True)
+    freqDir = os.path.join(outputPath, 'freq')
+    os.makedirs(freqDir, exist_ok=True)
 
     imageFiles = []
     for root, dirs, files in os.walk(inputPath, topdown=False):
